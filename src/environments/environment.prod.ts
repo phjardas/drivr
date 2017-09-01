1314a7ce2227ae56fffbb128cb6f925a902f8195
Philipp Jardas <philipp@bd4travel.com>
2017-09-01 17:10:35 +0200

    configured firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD5IlsvQ5Ie2mUAsbENSc_BPpLpmF-yJzw',
+    authDomain: 'drivr-f620a.firebaseapp.com',
+    databaseURL: 'https://drivr-f620a.firebaseio.com',
+    projectId: 'drivr-f620a',
+    storageBucket: 'drivr-f620a.appspot.com',
+    messagingSenderId: '908979870448',
+  }
 };
