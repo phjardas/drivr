21c875973f3e79c6f61d78e73d7840cd71226327
Philipp Jardas <philipp@bd4travel.com>
2017-09-02 12:33:19 +0200

    first dashboard draft

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { User } from 'firebase/app';
+
+import { CarService, Car } from './car.service';
 
 @Component({
   selector: 'dashboard',
   templateUrl: 'dashboard.component.html'
 })
 export class DashboardComponent {
+  user: Observable<User>;
+  cars: Observable<Car[]>
+
+  constructor(private carService: CarService) {
+    this.user = carService.user;
+    this.cars = carService.cars;
+  }
 }
