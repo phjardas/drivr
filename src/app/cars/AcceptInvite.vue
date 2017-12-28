bf4707a5989f2fed6d69acc0b6b934717a6af50e
Philipp Jardas <philipp@jardas.de>
2017-12-29 00:54:36 +0100

    tweaked car share ui

--- src/app/cars/AcceptInvite.vue
+++ src/app/cars/AcceptInvite.vue
@@ -6,6 +6,39 @@
     <span>Error loading the invite: {{ invite.error.message }}</span>
   </md-snackbar>
 
+  <template v-else-if="userId === invite.entity.inviter.id">
+    <template v-if="invite.entity.acceptedAt">
+      <p>Your invitation for car</p>
+      <p class="md-display-1">{{ invite.entity.car.label }}</p>
+      <p>was accepted by</p>
+      <p class="md-display-1">{{ invite.entity.invitee.label }}</p>
+      <p>on {{ invite.entity.acceptedAt | moment('ll') }}.</p>
+    </template>
+
+    <template v-else>
+      <p class="md-display-1">{{ invite.entity.car.label }}</p>
+      <p>
+        Send this link to the person you want to share the car with.
+        They will then be able to record refuels for it.
+      </p>
+
+      <p v-if="shareAvailable">
+        <md-button class="md-icon-button md-primary" @click="share">
+          <md-icon>share</md-icon>
+        </md-button>
+      </p>
+      <p v-else>
+        <md-field>
+          <md-input v-model="inviteUrl" @focus="copyInviteLink" readonly />
+          <span v-if="inviteLinkCopied" class="md-helper-text">
+            <md-icon>link</md-icon>
+            copied to clipboard
+          </span>
+        </md-field>
+      </p>
+    </template>
+  </template>
+
   <div v-else style="text-align: center">
     <p class="md-display-1">{{ invite.entity.inviter.label }}</p>
     <p>has invited you to record refuels for the car</p>
@@ -44,10 +77,6 @@
       </p>
     </template>
 
-    <md-snackbar v-else-if="userId === invite.entity.inviter.id" md-position="center" :md-duration="Infinity" :md-active="true" md-persistent>
-      <span>You can not accept your own invitation.</span>
-    </md-snackbar>
-
     <div v-else>
       <md-button class="md-raised md-primary" @click="accept">
         Accept invitation
@@ -63,6 +92,12 @@ import Spinner from '../Spinner';
 
 export default {
   components: { Spinner },
+
+  data: () => ({
+    shareAvailable: !!navigator.share,
+    inviteUrl: document.location.href,
+    inviteLinkCopied: false,
+  }),
 
   computed: {
     ...mapState({
@@ -81,9 +116,21 @@ export default {
     accept() {
       this.acceptCarInvite({ inviteId: this.$route.params.id });
     },
+    share() {
+      navigator.share({
+        title: `drivr`,
+        text: `Please help me record refuels for my car ${this.invite.entity.car.label} on drivr`,
+        url: this.inviteUrl,
+      });
+    },
+    copyInviteLink($event) {
+      $event.target.select();
+      document.execCommand('copy');
+      this.inviteLinkCopied = true;
+    },
   },
 
-  beforeMount() {
+  created() {
     this.$store.dispatch('setPageTitle', { title: 'Accept invite' });
     this.loadCarInvite({ inviteId: this.$route.params.id });
   },
