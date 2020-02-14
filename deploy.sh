2161e75960b618d8d9791f96d833d4018fa5ddf5
Philipp Jardas <philipp@jardas.de>
2020-02-14 21:35:19 +0100

    Revert "deploy with hosting branch"
    This reverts commit 6b17484bf39c21bc0b095b1f7981d4c7ac53fa72.

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,17 @@
 #!/bin/bash -e
 
-echo
-echo "=== Deleting old publication"
+echo "Deleting old publication"
 rm -rf hosting
 mkdir hosting
 git worktree prune
 rm -rf .git/worktrees/hosting/
 
-echo
-echo "=== Checking out hosting branch"
+echo "Checking out hosting branch"
 git worktree add -B hosting hosting origin/hosting
 
-echo
-echo "=== Updating files"
+echo "Updating files"
 cp -r public/* hosting
 cd hosting
 git add --all
 git commit -m "publish"
-
-echo
-echo "=== Publishing"
-git push -u origin hosting
-yarn --immutable --immutable-cache --inline-builds
-yarn firebase deploy --token=${FIREBASE_API_TOKEN}
+#git push -u origin hosting
