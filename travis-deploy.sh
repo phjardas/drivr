cff4907e744d7c68e150b33bb860464078a931a3
Philipp Jardas <philipp@jardas.de>
2020-02-14 09:09:10 +0100

    Deploy to S3

--- travis-deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo "Deploying branch $TRAVIS_BRANCH to Firebase"
-  yarn firebase deploy --token=${FIREBASE_API_TOKEN}
-else
-  echo "Skipping Firebase deployment of branch $TRAVIS_BRANCH"
-fi
