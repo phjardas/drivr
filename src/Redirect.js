98e53d67d8216bce86e630e6459c90c7b2743180
Philipp Jardas <philipp@jardas.de>
2020-02-15 16:20:51 +0100

    remove logging

--- src/Redirect.js
+++ src/Redirect.js
@@ -4,7 +4,6 @@ import { useNavigate } from 'react-router-dom';
 export default function Redirect({ to }) {
   const navigate = useNavigate();
   useEffect(() => {
-    console.debug('redirect:', to);
     navigate(to);
   }, [navigate, to]);
   return null;
