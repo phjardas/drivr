66754e0254751ba8a31b264a2a1676a9a5de2293
Philipp Jardas <philipp@jardas.de>
2020-02-15 16:33:10 +0100

    charts: make smoothing configurable

--- src/pages/cars/_id/CarCharts.js
+++ src/pages/cars/_id/CarCharts.js
@@ -1,26 +1,49 @@
-import { Card, CardContent, Grid, Typography, useTheme } from '@material-ui/core';
+import { Card, CardContent, Grid, Slider, Typography, useTheme } from '@material-ui/core';
 import React, { useCallback, useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import { LineSeries, XAxis, XYPlot, YAxis } from 'react-vis';
 import 'react-vis/dist/style.css';
 import { useRefuels } from '../../../data';
 import DelayedProgress from '../../../DelayedProgress';
 
+const smoothingLocaleStorageKey = 'drivr:chart-smoothing';
+
 function Charts({ refuels }) {
+  const [smoothing, setSmoothing] = useState(parseFloat(localStorage.getItem(smoothingLocaleStorageKey) || '0.9', 10));
+  const updateSmoothing = useCallback(
+    (value) => {
+      localStorage.setItem(smoothingLocaleStorageKey, value.toString());
+      setSmoothing(value);
+    },
+    [setSmoothing]
+  );
+
   return (
     <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Settings smoothing={smoothing} setSmoothing={updateSmoothing} />
+      </Grid>
       <Grid item xs={12} md={6}>
         <MileageOverTimeChart refuels={refuels} />
       </Grid>
       <Grid item xs={12} md={6}>
-        <ConsumptionOverTimeChart refuels={refuels} />
+        <ConsumptionOverTimeChart refuels={refuels} smoothing={smoothing} />
       </Grid>
       <Grid item xs={12} md={6}>
-        <FuelPriceOverTimeChart refuels={refuels} />
+        <FuelPriceOverTimeChart refuels={refuels} smoothing={smoothing} />
       </Grid>
       <Grid item xs={12} md={6}>
-        <DistanceOverTimeChart refuels={refuels} />
+        <DistanceOverTimeChart refuels={refuels} smoothing={smoothing} />
       </Grid>
     </Grid>
+  );
+}
+
+function Settings({ smoothing, setSmoothing }) {
+  return (
+    <>
+      <Typography gutterBottom>Smoothing</Typography>
+      <Slider min={0} max={1} step={0.01} value={smoothing} onChange={(_, v) => setSmoothing(v)} />
+    </>
   );
 }
 
@@ -41,65 +64,41 @@ function MileageOverTimeChart({ refuels }) {
   );
 }
 
-function ConsumptionOverTimeChart({ refuels }) {
-  const { palette } = useTheme();
+function ConsumptionOverTimeChart({ refuels, smoothing }) {
   const data = useMemo(
     () => refuels.filter((refuel) => refuel.consumption).map((refuel) => ({ x: new Date(refuel.date.seconds * 1000), y: refuel.consumption * 100 })),
     [refuels]
   );
-  const dataAttenuated = useAttenuated(data, 0.1);
-
-  return (
-    <ChartCard title="Consumption over time">
-      {(props) => (
-        <XYPlot {...props} xType="time">
-          <XAxis />
-          <YAxis />
-          <LineSeries data={dataAttenuated} stroke={palette.primary.light} curve="basis" />
-          <LineSeries data={data} stroke={palette.primary.main} curve="basis" />
-        </XYPlot>
-      )}
-    </ChartCard>
-  );
+  return <SmoothedChart title="Consumption over time" data={data} smoothing={smoothing} />;
 }
 
-function FuelPriceOverTimeChart({ refuels }) {
-  const { palette } = useTheme();
+function FuelPriceOverTimeChart({ refuels, smoothing }) {
   const data = useMemo(
     () => refuels.filter((refuel) => refuel.pricePerLiter).map((refuel) => ({ x: new Date(refuel.date.seconds * 1000), y: refuel.pricePerLiter })),
     [refuels]
   );
-  const dataAttenuated = useAttenuated(data, 0.1);
-
-  return (
-    <ChartCard title="Fuel price over time">
-      {(props) => (
-        <XYPlot {...props} xType="time">
-          <XAxis />
-          <YAxis />
-          <LineSeries data={dataAttenuated} stroke={palette.primary.light} curve="basis" />
-          <LineSeries data={data} stroke={palette.primary.main} curve="basis" />
-        </XYPlot>
-      )}
-    </ChartCard>
-  );
+  return <SmoothedChart title="Price over time" data={data} smoothing={smoothing} />;
 }
 
-function DistanceOverTimeChart({ refuels }) {
-  const { palette } = useTheme();
+function DistanceOverTimeChart({ refuels, smoothing }) {
   const data = useMemo(() => refuels.filter((refuel) => refuel.distance).map((refuel) => ({ x: new Date(refuel.date.seconds * 1000), y: refuel.distance })), [
     refuels,
   ]);
-  const dataAttenuated = useAttenuated(data, 0.1);
+  return <SmoothedChart title="Distance over time" data={data} smoothing={smoothing} />;
+}
+
+function SmoothedChart({ data, title, smoothing }) {
+  const { palette } = useTheme();
+  const dataAttenuated = useAttenuated(data, smoothing);
 
   return (
-    <ChartCard title="Distance over time">
+    <ChartCard title={title}>
       {(props) => (
         <XYPlot {...props} xType="time">
           <XAxis />
           <YAxis />
-          <LineSeries data={dataAttenuated} stroke={palette.primary.light} curve="basis" />
-          <LineSeries data={data} stroke={palette.primary.main} curve="basis" />
+          <LineSeries data={data} stroke={palette.secondary.light} curve="basis" />
+          <LineSeries data={dataAttenuated} stroke={palette.primary.main} curve="basis" />
         </XYPlot>
       )}
     </ChartCard>
@@ -158,9 +157,9 @@ function debounce(fn, ms) {
   };
 }
 
-function useAttenuated(data, factor) {
-  return useMemo(() => data.reduce((agg, d) => [...agg, { x: d.x, y: agg.length ? agg[agg.length - 1].y * (1 - factor) + d.y * factor : d.y }], []), [
+function useAttenuated(data, smoothing) {
+  return useMemo(() => data.reduce((agg, d) => [...agg, { x: d.x, y: agg.length ? agg[agg.length - 1].y * smoothing + d.y * (1 - smoothing) : d.y }], []), [
     data,
-    factor,
+    smoothing,
   ]);
 }
